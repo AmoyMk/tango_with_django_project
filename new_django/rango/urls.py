7e1f9ccb652ebaee54aca5b8af1506f5417eef0b
AmoyMk <97642480+AmoyMk@users.noreply.github.com>
2022-02-18 01:57:25 +0000

    Update urls.py

--- new_django/rango/urls.py
+++ new_django/rango/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from rango import views
 app_name = 'rango'
 urlpatterns = [
-    path('', views.about, name='index'),
+    path('', views.index, name='index'),
     path('', views.about, name='about'),
 ]
