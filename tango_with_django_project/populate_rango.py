fc56358c8557666a67e06ffc6d6dccb485b5f568
AmoyMk <97642480+AmoyMk@users.noreply.github.com>
2022-02-22 14:08:53 +0000

    Update populate_rango.py

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -1,4 +1,8 @@
-import os
+
+
+
+    
+   import os
 os.environ.setdefault(  'DJANGO_SETTINGS_MODULE',
                             'tango_with_django_project.settings')
 
@@ -61,14 +65,17 @@ def add_page(cat, title, url, views=0):
     return p
 
 def add_cat(name, views=0, likes=0):
-    c = Category.objects.get_or_create(name=name)[0]
-    c.likes = likes
-    c.views = views
+    c = Category.objects.get_or_create(name=name, views=0, likes=0)[0]
+    if name == 'Python':
+        c.likes = 64
+        c.views = 128
+    elif name == 'Django':
+        c.likes = 32
+        c.views = 64
     c.save()
     return c
 
 # Start execution here!
 if __name__ == '__main__':
     print('Starting Rango population script...')
-    populate()
-
+    populate() 
