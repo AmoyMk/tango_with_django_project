aa81d84eed38be8ad85623d1a9c3334ba6ce4ae5
AmoyMk <97642480+AmoyMk@users.noreply.github.com>
2022-02-23 15:09:00 +0000

    Update urls.py

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -19,7 +19,9 @@ from django.urls import include
 from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 
+app_name = 'rango'
 urlpatterns = [
 
     path('', views.index, name='index'),
